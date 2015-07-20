233b54a46015ac00ec6ab077b6ccae287aa096e8
Patrick Montag <patrickmontag@Patricks-MacBook-Pro.local>
2015-07-19 22:29:35 -0500

    third commit

--- Countdown/ViewController.m
+++ Countdown/ViewController.m
@@ -23,6 +23,7 @@
     AVAudioPlayer *_zero;
     
     NSString *openString;
+    NSDateFormatter *formatter;
 }
 @end
 
@@ -42,33 +43,6 @@
 }
 
 -(void)updateClockLabel {
-    
-    self.destinationLabel.text = self.textField.text;
-    
-    // create formatter to display dates how we want
-    NSDateFormatter *fullFormat = [[NSDateFormatter alloc] init];
-    [fullFormat setDateFormat:@"EEE, yyyy-MM-dd HH:mm:ss"];
-    
-    NSString *clockString = [fullFormat stringFromDate:[NSDate date]];
-    
-    self.clockLabel.text = clockString;
-    
-    // create date object from user input
-    NSDate *destinationDate = [fullFormat dateFromString:self.textField.text];
-    
-    // create date 30 seconds ahead
-    NSDate *openTime = [destinationDate dateByAddingTimeInterval:30];
-    
-    openString = [fullFormat stringFromDate:openTime];
-    
-    // calculate difference in times
-    NSTimeInterval diff = [[NSDate date] timeIntervalSinceDate:destinationDate];
-    
-    // convert double back to string to display
-    NSString *countdownString = [NSString stringWithFormat:@"%.2f", diff];
-    self.countdownLabel.text = countdownString;
-    
-    [self performSelector:@selector(updateClockLabel) withObject:self afterDelay:0.001];
     
     if ([self.countdownLabel.text isEqualToString:@"-10.00"]) {
         [_ten play];
@@ -93,60 +67,87 @@
     } else if ([self.countdownLabel.text isEqualToString:@"0.00"]) {
         [_zero play];
     }
+    
+    self.destinationLabel.text = self.textField.text;
+    
+    // create formatter to display dates how we want
+    [formatter setDateFormat:@"EEE, yyyy-MM-dd HH:mm:ss"];
+    
+    NSString *clockString = [formatter stringFromDate:[NSDate date]];
+    
+    self.clockLabel.text = clockString;
+    
+    // create date object from user input
+    NSDate *destinationDate = [formatter dateFromString:self.textField.text];
+    
+    // create date 30 seconds ahead
+    NSDate *openTime = [destinationDate dateByAddingTimeInterval:30];
+    
+    openString = [formatter stringFromDate:openTime];
+    
+    // calculate difference in times
+    NSTimeInterval diff = [[NSDate date] timeIntervalSinceDate:destinationDate];
+    
+    // convert double back to string to display
+    NSString *countdownString = [NSString stringWithFormat:@"%.2f", diff];
+    self.countdownLabel.text = countdownString;
+    
+    [self performSelector:@selector(updateClockLabel) withObject:self afterDelay:0.0001];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-// 10
+    // load sounds, should be an array
+    // 10
     NSString *pathTen = [NSString stringWithFormat:@"%@/ten.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundTen = [NSURL fileURLWithPath:pathTen];
     _ten = [[AVAudioPlayer alloc] initWithContentsOfURL:soundTen error:nil];
-// 9
+    // 9
     NSString *pathNine = [NSString stringWithFormat:@"%@/nine.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundNine = [NSURL fileURLWithPath:pathNine];
     _nine = [[AVAudioPlayer alloc] initWithContentsOfURL:soundNine error:nil];
-// 8
+    // 8
     NSString *pathEight = [NSString stringWithFormat:@"%@/eight.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundEight = [NSURL fileURLWithPath:pathEight];
     _eight = [[AVAudioPlayer alloc] initWithContentsOfURL:soundEight error:nil];
-// 7
+    // 7
     NSString *pathSeven = [NSString stringWithFormat:@"%@/seven.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundSeven = [NSURL fileURLWithPath:pathSeven];
     _seven = [[AVAudioPlayer alloc] initWithContentsOfURL:soundSeven error:nil];
-// 6
+    // 6
     NSString *pathSix = [NSString stringWithFormat:@"%@/six.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundSix = [NSURL fileURLWithPath:pathSix];
     _six = [[AVAudioPlayer alloc] initWithContentsOfURL:soundSix error:nil];
-// 5
+    // 5
     NSString *pathFive = [NSString stringWithFormat:@"%@/five.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundFive = [NSURL fileURLWithPath:pathFive];
     _five = [[AVAudioPlayer alloc] initWithContentsOfURL:soundFive error:nil];
-// 4
+    // 4
     NSString *pathFour = [NSString stringWithFormat:@"%@/four.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundFour = [NSURL fileURLWithPath:pathFour];
     _four = [[AVAudioPlayer alloc] initWithContentsOfURL:soundFour error:nil];
-// 3
+    // 3
     NSString *pathThree = [NSString stringWithFormat:@"%@/three.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundThree = [NSURL fileURLWithPath:pathThree];
     _three = [[AVAudioPlayer alloc] initWithContentsOfURL:soundThree error:nil];
-// 2
+    // 2
     NSString *pathTwo = [NSString stringWithFormat:@"%@/two.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundTwo = [NSURL fileURLWithPath:pathTwo];
     _two = [[AVAudioPlayer alloc] initWithContentsOfURL:soundTwo error:nil];
-// 1
+    // 1
     NSString *pathOne = [NSString stringWithFormat:@"%@/one.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundOne = [NSURL fileURLWithPath:pathOne];
     _one = [[AVAudioPlayer alloc] initWithContentsOfURL:soundOne error:nil];
-// 0
+    // 0
     NSString *pathZero = [NSString stringWithFormat:@"%@/zero.wav", [[NSBundle mainBundle] resourcePath]];
     NSURL *soundZero = [NSURL fileURLWithPath:pathZero];
     _zero = [[AVAudioPlayer alloc] initWithContentsOfURL:soundZero error:nil];
     
     // display todays date in the input field
-    
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter = [[NSDateFormatter alloc] init];
     [formatter setDateFormat:@"EEE, yyyy-MM-dd HH:mm:ss"];
+    
     NSDate *todaysDate = [NSDate date];
     NSString *todaysString = [formatter stringFromDate:todaysDate];
     
